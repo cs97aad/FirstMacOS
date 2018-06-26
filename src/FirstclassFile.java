ccd7cf39b0f1c2a34ab2526515f0ba710dd04fc5
cs97aad <cs97aad@yahoo.co.uk>
2018-06-26 18:05:46 +0100

    branch2

---
+++ src/FirstclassFile.java
@@ -0,0 +1,9 @@
+
+public class FirstclassFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to JAVA first Program on MACos");
+	}
+
+}
